eaf0255e15f38dac644f50770bd00b5414d1cc6e
ye-geeee <yeaji9683@gmail.com>
2021-08-02 23:43:10 +0900

    changes condition to simple way

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
 
   return (
     <div className="App">
-      {name === '리액트' ? (
-        <h1>리액트입니다.</h1>) : 
-        (<h1>리액트가 아닙니다.</h1>)}
+      {name === '리액트' && (<h1>리액트입니다.</h1>)}
     </div>
   );
 }
