fcb89b91121bc5a1c898a64d318fd2575abcda4e
ye-geeee <yeaji9683@gmail.com>
2021-08-03 05:22:41 +0900

    apply simple style

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import './App.css';
 
 function App() {
   const name = '리액트';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: '48x',
+    fontWeight: 'bold',
+    padding: 16
+  };
 
   return (
-    <div className="App">
+    <div className="App" style={style}>
       {name === '리액트' && (<h1>리액트입니다.</h1>)}
     </div>
   );
