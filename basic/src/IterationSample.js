edd9e95f1cf55dff6f6fb670e31accb9b10c83a0
ye-geeee <yeaji9683@gmail.com>
2021-08-18 06:11:53 +0900

    chatp 6.4.2 useState using map()

--- basic/src/IterationSample.js
+++ basic/src/IterationSample.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const IterationSample = () => {
-    const names = ['눈사람', '얼음', '눈', '바람'];
-    const nameList = names.map((name, index) => <li key={index}>{name}</li>);
+    const [names, setNames] = useState([
+        { id: 1, text: '눈사람' },
+        { id: 2, text: '얼음' },
+        { id: 3, text: '눈' },
+        { id: 4, text: '바람' }
+    ]);
+    const [inputText, setInputText] = useState('');
+    const [nextId, setNextId] = useState(5);
+    const nameList = names.map(name => <li key={name.id}>{name.text}</li>);
+
+    const onChange = e => setInputText(e.target.value);
+    const onClick = () => {
+        const nextNames = names.concat({
+            id: nextId,
+            text: inputText
+        });
+        setNextId(nextId + 1);
+        setNames(nextNames);
+        setInputText('');
+    };
 
     return (
-        <div>
+        <>
+            <input value={inputText} onChange={onChange}></input>
+            <button onClick={onClick}>추가</button>
             <ul>
                 {nameList}
             </ul>
-        </div>
+        </>
     )
 }
 
