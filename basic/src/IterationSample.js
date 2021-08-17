88b148272b6d59f76102e2233cd80e67f0c1496e
ye-geeee <yeaji9683@gmail.com>
2021-08-18 05:49:17 +0900

    move code to basic folder

---
+++ basic/src/IterationSample.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const IterationSample = () => {
+    return (
+        <div>
+            <ul>
+                <li>눈사람</li>
+                <li>얼음</li>
+                <li>눈</li>
+                <li>바람</li>
+            </ul>
+        </div>
+    )
+}
+
+export default IterationSample;
