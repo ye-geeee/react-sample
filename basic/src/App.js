5b4d7982ec08a55266ed88fc3596a10436e130a9
ye-geeee <yeaji9683@gmail.com>
2021-08-27 07:29:23 +0900

    chap 8.3.1 useReducer sample code

--- basic/src/App.js
+++ basic/src/App.js
@@ -19,16 +19,7 @@ const App = () => {
   const [visible, setVisible] = useState(false);
 
   return (
-    <div>
-      <button onClick={() => {
-        setVisible(!visible);
-      }}
-      >
-        {visible ? 'hide' : 'show'}
-      </button>
-      <hr />
-      {visible && <Info />}
-    </div>
+    <Counter />
   );
 };
 
--- basic/src/Counter.js
+++ basic/src/Counter.js
@@ -1,15 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { value: state.value + 1 };
+    case 'DECREMENT':
+      return { value: state.value - 1 };
+    default:
+      return state;
+  }
+}
 
 const Counter = () => {
-  const [value, setValue] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { value: 0 });
 
   return (
     <div>
       <p>
-        {value}
+        {state.value}
       </p>
-      <button onClick={() => setValue(value + 1)}>+1</button>
-      <button onClick={() => setValue(value - 1)}>-1</button>
+      <button onClick={() => dispatch({ type: 'INCREMENT' })}>+1</button>
+      <button onClick={() => dispatch({ type: 'DECREMENT' })}>-1</button>
     </div>
   );
 };
