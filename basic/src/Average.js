f91e20b2ab1184b4c2d7eeb7179a03c41796346e
ye-geeee <yeaji9683@gmail.com>
2021-09-10 06:30:56 +0900

    chap 8.5 useCallback sample code

--- basic/src/Average.js
+++ basic/src/Average.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useCallback } from 'react';
 
 const getAverage = numbers => {
     console.log('Calculating average...');
@@ -11,15 +11,15 @@ const Average = () => {
     const [list, setList] = useState([]);
     const [number, setNumber] = useState('');
 
-    const onChange = e => {
+    const onChange = useCallback(e => {
         setNumber(e.target.value);
-    };
+    }, []);
 
-    const onInsert = e => {
+    const onInsert = useCallback(e => {
         const nextList = list.concat(parseInt(number));
         setList(nextList);
         setNumber('');
-    };
+    }, [number, list]);
 
     const avg = useMemo(() => getAverage(list), [list]);
 
