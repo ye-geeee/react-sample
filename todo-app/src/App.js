f9ef5b2244b99705bd78c52c380e970e8ff55d46
ye-geeee <yeaji9683@gmail.com>
2021-09-20 11:34:52 +0900

    chap 10.3.1 add todo list

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: 'react1',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: 'react2',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: 'react3',
+      checked: false,
+    },
+  ]);
+
   return (
     <TodoTemplate>
       <TodoInsert />
-      <TodoList />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 };
