bcc51939f845ca19f8d091025d37fbc254077eaa
ye-geeee <yeaji9683@gmail.com>
2021-09-20 13:00:02 +0900

    chap 10.3.4 feat: toggle checkbox

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -36,10 +36,26 @@ const App = () => {
     setTodos(todos.filter((todo) => todo.id !== id));
   });
 
+  const onToggle = useCallback(
+    (id) => {
+      setTodos(
+        todos.map((todo) =>
+          todo.id === id
+            ? {
+                ...todo,
+                checked: !todo.checked,
+              }
+            : todo,
+        ),
+      );
+    },
+    [todos],
+  );
+
   return (
     <TodoTemplate>
       <TodoInsert onInsert={onInsert} />
-      <TodoList todos={todos} onRemove={onRemove} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </TodoTemplate>
   );
 };
