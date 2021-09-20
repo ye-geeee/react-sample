96782df7c3178d63284bd756ba3e6a6f05569380
ye-geeee <yeaji9683@gmail.com>
2021-09-20 11:52:45 +0900

    chap 10.3.2 feat: add todo task

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
@@ -22,9 +22,19 @@ const App = () => {
     },
   ]);
 
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    (text) => {
+      const todo = { id: nextId.current, text, checked: false };
+      setTodos(todos.concat(todo));
+    },
+    [todos],
+  );
+
   return (
     <TodoTemplate>
-      <TodoInsert />
+      <TodoInsert onInsert={onInsert} />
       <TodoList todos={todos} />
     </TodoTemplate>
   );
